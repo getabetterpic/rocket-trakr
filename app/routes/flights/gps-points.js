5478cf172cad03ce3844c6ebd119f9158d2f2e4e
getabetterpic <dan@danandjensmith.com>
2016-04-29 09:33:18 -0400

    Add flight description and export button to gps points

--- app/routes/flights/gps-points.js
+++ app/routes/flights/gps-points.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.query('gps-point', { flight: params.flight_id });
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('flight', model.get('firstObject.flight'));
   }
 });
