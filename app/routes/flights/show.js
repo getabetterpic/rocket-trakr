56221f43b3b87805e370ae5aa1a52215f39085e7
getabetterpic <dan@danandjensmith.com>
2016-04-29 13:37:09 -0400

    Add delete button for flight

--- app/routes/flights/show.js
+++ app/routes/flights/show.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.find('flight', params.flight_id);
+  },
+  actions: {
+    deleteFlight(flight) {
+      flight.destroyRecord().then(() => {
+        this.transitionTo('flights.index');
+      });
+    }
   }
 });
